7fad28b4fe1951370bec26693aca4967b69a5e26
Anselme Donato <anselme.donato2@gmail.com>
2023-12-08 13:29:33 +0200

    (removing cache from git and) handling no more assignment

--- programming-api/services/programmingAssignmentService.js
+++ programming-api/services/programmingAssignmentService.js
@@ -6,6 +6,14 @@ const findAll = async () => {
 
 const findById = async (id) => {
   const results = await sql`SELECT * FROM programming_assignments WHERE id = ${ id };`;
+	if(results.length == 0){
+		console.log("Not found"); 
+		return {
+			id: id,
+			title: "Rest", 
+			handout: "Looks like you have done all the assignments for now: you can take a well deserved break. See you later."
+		}; 
+	}
 	return results[0]; 
 };
 
