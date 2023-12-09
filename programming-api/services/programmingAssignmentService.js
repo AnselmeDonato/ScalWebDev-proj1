bed3aca3a1d5631aad051e1f55ccfe9e8c019136
Anselme Donato <anselme.donato2@gmail.com>
2023-12-09 21:19:32 +0200

    Cleaning

--- programming-api/services/programmingAssignmentService.js
+++ programming-api/services/programmingAssignmentService.js
@@ -7,7 +7,7 @@ const findAll = async () => {
 const findById = async (id) => {
   const results = await sql`SELECT * FROM programming_assignments WHERE id = ${ id };`;
 
-	// We consider that "id doesn't exist" is always "the user has completed all tasks", and inform the user with a custom "assignment"
+	// We consider that <id not found> is always <the user has completed all tasks>, and inform the user with a custom "assignment"
 	if(results.length == 0){
 		return {
 			id: id,
